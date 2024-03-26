a2c0c2658b30286b621c6eced98346adcb36c4f4
Paulo Henrique <ph55@hotmail.com>
2024-03-25 21:11:47 -0300

    Update desafio_43.py
    Add a função que pega a idade e trata a informação coletada
    
    Add a função que pega o sexo e e trata a informação coletada

--- python- challenges/desafio_43.py
+++ python- challenges/desafio_43.py
@@ -8,14 +8,45 @@ import os
 import re
 
 def get_name():
-    while True:
+    cont = True
+    while cont:
         name = input('Nome: ')
         if re.match("^[a-zA-Z ]+$", name):
+            cont = False
             return name
         else:
-            print('Nome inválido! Por favor, digite apenas letras e espaços.')
+            print('Nome inválido! Pressione ENTER para tentar novamente')
+            input()
+
+def get_year():
+    cont = True
+    while cont:
+        try:
+            year = int(input('Idade: '))
+        except Exception as e:
+            print('Valor Inválido! Pressio ENTER para tentar novamente')
+        else:
+            cont = False
+            return year
+    
+def get_sex():
+    cont = True
+    while cont:
+        sex = input('Sexo [M/F]: ').upper()
+        if re.match("^[a-zA-Z ]+$", sex) and sex == 'M' or sex == 'F':
+            cont = False
+            return sex
+        else:
+            print('Nome inválido! Pressione ENTER para tentar novamente')
+            input()
+        
+
 
 nome_usuario = get_name()
+idade_usuario = get_year()
+sexo_usuario = get_sex()
+
+print(nome_usuario, idade_usuario, sexo_usuario)
 
 
 
