d5ae78dfa0e285a8efb084a237e68de3022267fc
Paulo Henrique <ph55@hotmail.com>
2024-03-26 16:14:53 -0300

    Update desafio43_py
    Add as funcionalidades
     -> Coleta a informação do homem mais velho
    -> Coleta a informação das mulheres com menos de 20 anos
    -> Exibi os resultados solicitados

--- python- challenges/desafio_43.py
+++ python- challenges/desafio_43.py
@@ -7,6 +7,7 @@ Desenvola um programa que leia o nome, idade e sexo de 4 pessoas. No final o cod
 import os
 import re
 
+# Pega nome
 def get_name():
     os.system('cls')
     print('-- SISTEMA DE CADASTRO --')
@@ -22,6 +23,7 @@ def get_name():
             input()
             os.system('cls')
 
+# Pega idade
 def get_year():
     cont = True
     while cont:
@@ -35,7 +37,8 @@ def get_year():
         else:
             cont = False
             return year
-    
+
+# Pega Sexo  
 def get_sex():
     cont = True
     while cont:
@@ -49,12 +52,44 @@ def get_sex():
             input()
             os.system('cls')
 
+# Calcula Média de Idade
 def calculate_average_age(age_list):
     age_media = sum(age_list) / len(age_list)
     return age_media
 
 
+cont = 1
+year_list = []
+old_men = []
+woman_20 = []
 
+
+while cont <= 4:
+    name = get_name()
+    year = get_year()
+    year_list.append(year)
+    sex = get_sex()
+
+    if sex == 'M':
+        if len(old_men) == 0:
+            old_men.append(year)
+        else:
+            if  year > old_men[0]:
+                old_men[0] = year
+
+    if sex == 'F':
+        if year < 20:
+            woman_20.append(year)
+
+    cont += 1
+
+os.system('cls')
+print(f'A média de idade do grupo é de {calculate_average_age(year_list)} anos')
+print(f'A maior idade registrada de um hom é {old_men[0]} anos')
+print(f'A quantidade de mulheres com menos de 20 anos é {len(woman_20)}\n\n')
+
+
+    
 
 
 
