f68c9e9a65a307bdabdb70e056e2544134226506
Paulo Henrique <ph55@hotmail.com>
2024-04-01 22:16:48 -0300

    update desafio_47.py
    add tratamento de erro no código

--- python- challenges/desafio_47.py
+++ python- challenges/desafio_47.py
@@ -19,18 +19,25 @@ num_list = []
 while cont:
     os.system('cls')
     print('CASO QUEIRA FINALIZAR PRESSIONE 999\n')
-    num = int(input('Insira o valor: '))
-    loading()
-    if num != 999:
-        num_list.append(num)
-    else:
-        num_size = len(num_list)
-        num_sum = sum(num_list)
+    try:
+        num = int(input('Insira o valor: '))
+    except Exception as e:
         os.system('cls')
-        print('=====================================================')
-        print(f'A quantidade de números digitados foi {num_size}')
-        print(f'E a soma entre eles é {num_sum}')
-        print('=====================================================\n\n')
-        cont = False
+        print('Valor Inválido!!')
+        time.sleep(0.5)
+        os.system('cls')
+    else:
+        loading()
+        if num != 999:
+            num_list.append(num)
+        else:
+            num_size = len(num_list)
+            num_sum = sum(num_list)
+            os.system('cls')
+            print('=====================================================')
+            print(f'A quantidade de números digitados foi {num_size}')
+            print(f'E a soma entre eles é {num_sum}')
+            print('=====================================================\n\n')
+            cont = False
 
 
