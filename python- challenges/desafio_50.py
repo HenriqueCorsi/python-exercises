8920e0ecc7673cada61586af56d703788c15c7c6
Paulo Henrique <ph55@hotmail.com>
2024-04-03 23:49:53 -0300

    update desafio_50.py
    Add o opção de escolher o número através do input
    Add elementos gráficos
    Add Menu de Bem Vindo

--- python- challenges/desafio_50.py
+++ python- challenges/desafio_50.py
@@ -11,38 +11,56 @@ def loading():
     time.sleep(1)
     os.system('cls')
 
-os.system('cls')
-select_user = input('Ímpar ou Par ? [I/P]').lower()
+def menu():
+    os.system('cls')
+    print('============================================')
+    print('          BEM VINDO AO ÍMPAR OU PAR          ')
+    print('============================================')
+
+menu()
+select_user = input('\nVocê escolhe ÍMPAR ou PAR ? [I/P]').lower()
 loading()
 cpu_num = random.randint(0, 50)
-user_num = 20
+user_num = int(input('Insira um Valor: '))
 result = cpu_num + user_num
 
 if select_user == 'i' and result % 2 != 0:#Usuario Ganha
     loading()
     print(f'Você: {user_num}')
+    print('============================================')
     print(f'CPU: {cpu_num}')
+    print('============================================')
     print(f'Soma: {result}')
-
+    print('============================================')
     print(f'{result} é ÍMPAR vc GANHOU!!')
+    print('============================================')
 elif select_user == 'i' and result % 2 == 0:#Usuario Perde
     loading()
     print(f'Você: {user_num}')
+    print('============================================')
     print(f'CPU: {cpu_num}')
+    print('============================================')
     print(f'Soma: {result}')
-
+    print('============================================')
     print(f'{result} não é ÍMPAR vc PERDEU!!')
+    print('============================================')
 elif select_user == 'p' and result % 2 == 0:#Usuario Ganha
     loading()
     print(f'Você: {user_num}')
+    print('============================================')
     print(f'CPU: {cpu_num}')
+    print('============================================')
     print(f'Soma: {result}')
-
-    print(f'{result} é PAR vc GANHOU!!')#Usuario Perde
-elif select_user == 'p' and result % 2 != 0:
+    print('============================================')
+    print(f'{result} é PAR vc GANHOU!!')
+    print('============================================')
+elif select_user == 'p' and result % 2 != 0:#Usuario Perde
     loading()
     print(f'Você: {user_num}')
+    print('============================================')
     print(f'CPU: {cpu_num}')
+    print('============================================')
     print(f'Soma: {result}')
-
+    print('============================================')
     print(f'{result} não é PAR vc PERDEU!!')
+    print('============================================')
