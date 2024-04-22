431c2d2510b2a5bd5d1c2fd9297906642b286721
Paulo Henrique <ph55@hotmail.com>
2024-04-22 10:08:14 -0300

    Update desafio_55.py

--- python- challenges/desafio_55.py
+++ python- challenges/desafio_55.py
@@ -5,27 +5,27 @@ dentro de um arquivo TXT. Crie tambem a opção de vizualizar os dados cadastrad
 from modulos_desafio_55.feature import *
 
 while True:
-    menu()
+    menu() # Chama Menu
     try:
-        select_menu = int(input('\n'))
+        select_menu = int(input('\n')) # Input Usuário
     except Exception as e:
         system('cls')
         print('Valor Inválido!')
         sleep(1)
     else:
-        if select_menu == 1:
-            nome = pega_nome()
+        if select_menu == 1: # Cadastro de nome e idade
+            nome = pega_nome() 
             idade = pega_idade()
 
             cadastra_dados(nome, idade)
-        elif select_menu == 2:
+        elif select_menu == 2: # Vizualizar Dados cadastrados
             vizualizar_dados()
-        elif select_menu == 3:
+        elif select_menu == 3: # Sair
             system('cls')
-            print('Saindo....')
+            print('Até Breve!!')
             sleep(1)
             break
-        else:
+        else: # Opção Inválida
             system('cls')
             print('Valor Inválido!')
             sleep(1)
